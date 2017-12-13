c4cb55e09008ff3ff5295e7a89aea15b05395ce1
ElectronicManuel <sedlaceke@bzz.ch>
2017-12-13 17:04:06 +0100

    adding auth

---
+++ client/ui/js/NavigationBar.js
@@ -0,0 +1,30 @@
+var navs = [
+    {
+        name: 'home',
+        route: '/',
+        displayName: 'Home',
+        icon: null
+    },
+    {
+        name: 'protokoll',
+        route: '/protokoll',
+        displayName: 'Protokoll',
+        icon: null
+    },
+    {
+        name: 'upload',
+        route: '/upload',
+        displayName: '',
+        icon: 'file_upload'
+    }
+];
+
+Template.Routes.helpers({
+    navigations: () => {
+        return navs;
+    }
+});
+
+Template.NavigationBar.onRendered(() => {
+    $(".button-collapse").sideNav();
+});
