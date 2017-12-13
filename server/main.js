8a5314a3f25c1e85d623d4a25f57e501d98153b5
ElectronicManuel <sedlaceke@bzz.ch>
2017-12-13 17:54:34 +0100

    configuring google auth

--- server/main.js
+++ server/main.js
@@ -2,13 +2,13 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
     // code to run on server at startup
-    ServiceConfiguration.configurations.upsert({
+    ServiceConfiguration.configurations.remove({
         service: "google"
-    }, {
-            $set: {
-                appId: Meteor.settings.google.appId,
-                loginStyle: "popup",
-                secret: Meteor.settings.google.secret
-            }
-        });
+    });
+    ServiceConfiguration.configurations.insert({
+        service: "google",
+        clientId: Meteor.settings.google.appId,
+        loginStyle: "popup",
+        secret: Meteor.settings.google.secret
+    });
 });
