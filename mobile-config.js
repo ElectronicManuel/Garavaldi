fb3867d9a166dc8d001c729d6a5a9ebb11a92e25
ElectronicManuel <sedlaceke@bzz.ch>
2018-01-23 20:12:58 +0100

    minor changes

--- mobile-config.js
+++ mobile-config.js
@@ -1,8 +1,16 @@
+App.info({
+    id: 'ch.crosssitefeeding.egalerie',
+    name: 'eGalerie',
+    version: "0.0.1"
+});
+
 App.accessRule('https://*.crosssitefeeding.ch');
 
 App.accessRule('https://*.googleapis.com');
 
 App.accessRule('https://*.gstatic.com');
+
+App.accessRule('https://*.google.com');
 
 App.icons({
     'android_mdpi': 'public/favicon.png',
