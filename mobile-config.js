35bc9008351b2410b903ad6cd8b4736e6c4fec61
ElectronicManuel <sedlaceke@bzz.ch>
2018-01-23 17:38:27 +0100

    mobile functionality

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+App.accessRule('https://*.crosssitefeeding.ch');
+
+App.accessRule('https://*.googleapis.com');
+
+App.accessRule('https://*.gstatic.com');
+
+App.icons({
+    'android_mdpi': 'public/favicon.png',
+    'android_hdpi': 'public/favicon.png',
+    'android_xhdpi': 'public/favicon.png',
+    'android_xxhdpi': 'public/favicon.png',
+    'android_xxxhdpi': 'public/favicon.png',
+});
