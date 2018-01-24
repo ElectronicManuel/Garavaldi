dd15e36c3101cc001635739257081914ab5d8a31
ElectronicManuel <sedlaceke@bzz.ch>
2018-01-25 00:03:46 +0100

    changing urls

--- client/ui/page/install/Install.js
+++ client/ui/page/install/Install.js
@@ -1,6 +1,6 @@
 Template.Install.helpers({
     versions: () => {
-        return Versions.find({}, {$sort: {label: 1}});
+        return Versions.find({}, {$sort: {updatedAt: -1}});
     },
     dateFormat: (date) => {
         return momentReactive(date).fromNow();
