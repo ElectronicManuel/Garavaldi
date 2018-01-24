46a1638911fa7d806ccfb57e0f10e3eae3f9bd4e
ElectronicManuel <sedlaceke@bzz.ch>
2018-01-24 11:39:13 +0100

    properly displaying version date

--- client/ui/page/install/Install.js
+++ client/ui/page/install/Install.js
@@ -1,5 +1,9 @@
 Template.Install.helpers({
     versions: () => {
-        return Versions.find({});
+        return Versions.find({}, {$sort: {label: 1}});
+    },
+    dateFormat: (date) => {
+        moment.locale('de');
+        return moment(date).fromNow();
     }
 });
